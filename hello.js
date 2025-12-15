c5d22f6ac3df643fa133065af2f5ef9bd2deadc2
betasammy <betacomputer2005@gmail.com>
2025-12-15 12:11:42 +0100

    Log HOMEBREW_PREFIX environment variable

--- hello.js
+++ hello.js
@@ -1,5 +1,7 @@
 const { createBook, plutobookBuildInfo } = require('plutoprint')
 
+console.log(process.env.HOMEBREW_PREFIX);
+
 book = createBook({size:'a3'})
 book.loadUrl('https://en.wikipedia.org/wiki/Main_Page')
 book.writeToPdf('hello.pdf')
